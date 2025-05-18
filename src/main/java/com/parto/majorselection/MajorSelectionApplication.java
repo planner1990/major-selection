ca3bdd25d1c55139d4e7251cbdc16f27610208df
hosseini <m.hosseini@asredanesh.com>
2025-05-18 14:19:47 +0330

    first commit

---
+++ src/main/java/com/parto/majorselection/MajorSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.parto.majorselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorSelectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parto/majorselection/MajorSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parto.majorselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorSelectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

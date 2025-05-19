549a2acf4b0c4d50d18eebad6e9582ce951ca75c
hosseini <m.hosseini@asredanesh.com>
2025-05-19 15:17:31 +0330

    Add RecommendationController and services

--- src/main/java/com/parto/majorselection/controller/RecommendationController.java
+++ src/main/java/com/parto/majorselection/controller/RecommendationController.java
@@ -2,6 +2,7 @@ package com.parto.majorselection.controller;
 
 import com.parto.majorselection.model.dto.RecommendedField;
 import com.parto.majorselection.model.request.RecommendationRequest;
+import com.parto.majorselection.model.response.BaseResponse;
 import com.parto.majorselection.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,9 @@ public class RecommendationController {
     private final RecommendationService recommendationService;
 
     @GetMapping
-    public List<RecommendedField> getRecommendations(@ModelAttribute RecommendationRequest request) {
-        return recommendationService.findRecommendations(request);
+    public BaseResponse<List<RecommendedField>> getRecommendations(@ModelAttribute RecommendationRequest request) {
+        List<RecommendedField> results = recommendationService.findRecommendations(request);
+        return new BaseResponse<>(true, "Recommendations fetched successfully", results);
     }
 
 
---
+++ src/main/java/com/parto/majorselection/model/response/BaseResponse.java
@@ -0,0 +1,12 @@
+package com.parto.majorselection.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BaseResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+}

1bf00816c668ee4be635b4b169055b464a4b77dc
hosseini <m.hosseini@asredanesh.com>
2025-05-20 08:14:23 +0330

    Add RecommendationController and services

--- src/main/java/com/parto/majorselection/bootstrap/RecommendationImporter.java
+++ src/main/java/com/parto/majorselection/bootstrap/RecommendationImporter.java
@@ -9,6 +9,7 @@ import com.parto.majorselection.model.entity.RecommendationResult;
 import com.parto.majorselection.repository.RecommendationResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
